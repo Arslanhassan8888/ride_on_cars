3e5b56d0ccb5cdcb26fdcc2ecfaddd6624e326aa
Arslanhassan8888 <arslanarslan84@yahoo.com>
2026-04-01 16:18:08 +0100

    Add transparent sticky navbar effect on scroll using JavaScript and CSS

--- index.php
+++ index.php
@@ -43,7 +43,17 @@
 <?php include 'footer.php'; ?>
 
 
+<script>
+window.addEventListener("scroll", function() {
+    const header = document.querySelector(".site-header");
 
+    if (window.scrollY > 0) {
+        header.classList.add("scrolled");
+    } else {
+        header.classList.remove("scrolled");
+    }
+});
+</script>
 </body>
 </html>
 
