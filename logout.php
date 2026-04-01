749b927893802c1be2c89fa3e0101ee11fb8af8a
Arslanhassan8888 <arslanarslan84@yahoo.com>
2026-04-01 20:22:04 +0100

    Logout securely destroys session data and removes session cookies to prevent session reuse

--- logout.php
+++ logout.php
@@ -1,5 +1,17 @@
 <?php
 session_start();
+
+// Unset all session variables
+$_SESSION = [];
+
+// Destroy session
 session_destroy();
 
+// Delete session cookie
+if (ini_get("session.use_cookies")) {
+    setcookie(session_name(), '', time() - 42000, '/');
+}
+
+// Redirect
 header("Location: index.php");
+exit();
